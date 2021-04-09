05ed0eebe46954056e9df21349536ba47d239e55
dmitriyperetoka <67266588+dmitriyperetoka@users.noreply.github.com>
2021-04-09 07:52:58 +0300

    Update urls.py

--- taskmanager/api/urls.py
+++ taskmanager/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView,
+)
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -13,5 +15,7 @@ router_v1.register('tasks', views.TaskViewSet, basename='tasks')
 
 urlpatterns = [
     path('v1/token', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('vq/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
     path('v1/', include(router_v1.urls)),
 ]
